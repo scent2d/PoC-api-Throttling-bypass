825e432424bf69017421e46ed89240360dab57fb
scent2d <45614318+scent2d@users.noreply.github.com>
2022-01-10 17:41:08 +0900

    Study

---
+++ testserver/quickstart/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from quickstart.serializers import UserSerializer, GroupSerializer
+from rest_framework.response import Response
+from rest_framework.throttling import UserRateThrottle
+from rest_framework.views import APIView
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
