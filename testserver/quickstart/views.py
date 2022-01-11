1d9d4ce4b9a8291eb8c2223a49fe215cc323812f
scent2d <45614318+scent2d@users.noreply.github.com>
2022-01-11 17:47:34 +0900

    change

--- testserver/quickstart/views.py
+++ testserver/quickstart/views.py
@@ -10,6 +10,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+    
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
